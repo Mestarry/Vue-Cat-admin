1844e1cff6f3d6198bfbeb4a01263166c6445a7c
Mestarry <164940880@qq.com>
2019-11-26 22:01:07 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import ElementUI from 'element-ui' // element-ui
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './styles/global.scss' // global css
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import './icons' // icon
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
