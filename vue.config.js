26244446345d9dc77883dbcb3e39834232c30f70
Mestarry <164940880@qq.com>
2019-12-07 08:46:09 +0800

    修复icon不显示

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,30 @@
+const path = require('path')
+const port = process.env.port || process.env.npm_config_port || 2019
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    port: port,
+    open: true
+  },
+  chainWebpack(config) {
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+  }
 }
