9fe9133207337835530d9240b91052c5bc6e5a22
Mestarry <164940880@qq.com>
2019-12-10 21:37:54 +0800

    新增：设置主题

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -1,12 +1,35 @@
-const settings = {
-  state: {
-    sidebarLogo: true,
-    tagsView: true
-  },
-  mutations: {
-  },
-  actions: {
+import variables from '@/styles/element-variables.scss'
+import defaultSettings from '@/settings'
+
+const { showSettings, tagsView, fixedHeader, sidebarLogo } = defaultSettings
+
+const state = {
+  theme: variables.theme,
+  showSettings: showSettings,
+  tagsView: tagsView,
+  fixedHeader: fixedHeader,
+  sidebarLogo: sidebarLogo
+}
+
+const mutations = {
+  CHANGE_SETTING: (state, { key, value }) => {
+    if (state.hasOwnProperty(key)) {
+      state[key] = value
+    }
   }
+}
+
+const actions = {
+  changeSetting({ commit }, data) {
+    commit('CHANGE_SETTING', data)
+  }
+}
+
+const settings = {
+  namespaced: true,
+  state,
+  mutations,
+  actions
 }
 
 export default settings
