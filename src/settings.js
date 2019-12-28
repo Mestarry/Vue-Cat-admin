868258e54c08a1cd5bd77dba35958fc2e96c2274
Mestarry <164940880@qq.com>
2019-12-28 15:30:07 +0800

    修改：生产环境不显示设置组件

--- src/settings.js
+++ src/settings.js
@@ -5,7 +5,7 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the settings right-panel
    */
-  showSettings: true,
+  showSettings: process.env.NODE_ENV !== 'production',
 
   /**
    * @type {boolean} true | false
